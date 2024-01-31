4ff89d1c098c093f1fb35d67818a283007dab6eb
Isabelle-Grimesey <isabellegrimesey@gmail.com>
2024-01-31 05:07:38 -0600

    Outlined main feature
    Created GUI and Core files, Divided GUI into data node sections and a section for an execute button, Declared core function and decided on data format for data nodes

---
+++ backend.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# TODO: create data nodes
+# let food items be a dataframe
+# let macro goals be a json
+# let portions be a dataframe
+# set up a task that has food items and macro goals as input, and portions as output, and references calculate_portions_for_3 as a function
+def calculate_portions_for_3():
+    pass
